c98e02969c96c5029526d0a7bbc5fa8fdfb824de
Philip Mousseau <phil@chmgraphics.com>
2015-12-09 14:31:48 +0000

    Added mailer and view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,3 +1,10 @@
 class ContactMailer < ActionMailer::Base
+    default to: 'miscstuff@anniesinks.com'    
     
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = doby
+        
+        mail(from: email, subject: 'Contact Form Message')
 end
